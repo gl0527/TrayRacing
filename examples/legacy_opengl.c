55c7eee9e4bfe439f347194ea4e7a9fa2b055d8f
gl0527 <gurzolajos27@gmail.com>
2026-09-05 16:47:37 +0200

    Deleted old placeholders

--- examples/legacy_opengl.c
+++
@@ -1,4 +0,0 @@
-# moved: source was reorganized into examples/legacy_opengl/legacy_opengl.c
-# kept for history; use the file under examples/legacy_opengl/ instead
-
-/* This file was moved to examples/legacy_opengl/legacy_opengl.c */
--- examples/nogui.c
+++
@@ -1,4 +0,0 @@
-# moved: source was reorganized into examples/nogui/nogui.c
-# kept for history; use the file under examples/nogui/ instead
-
-/* This file was moved to examples/nogui/nogui.c */
